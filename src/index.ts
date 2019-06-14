07b550a7e29802c420e8a2f92c7159ada116a411
Andy Gonzalez <andy.gonzalez@sap.com>
2019-06-14 15:36:11 -0400

    add timer

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
 import { Reconciler } from './Reconciler';
+import chalk from 'chalk';
+import now from 'performance-now';
 
 const l1 = [5, 3, 4];
 const l2 = [6, 4, 3, 10];
 
+var start = now();
 console.log(Reconciler.reconcile(l1, l2));
+var end = now();
+console.log(`${chalk.blue('That took: ')} ${(end - start).toFixed(3)} ms.`);
