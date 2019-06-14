6f6e1da72f697319ee99bbf435fea8c33cb3ad44
Andy Gonzalez <andy.gonzalez@sap.com>
2019-06-14 15:44:15 -0400

    done done

--- README.md
+++ README.md
@@ -1,8 +1,14 @@
 # array-reconciler
 
 ```
+Brute force:
 Reconciling [5,3,4] with [6,4,3,10]:
 In the first but not the second: 5
 In the second but not the first: 6,10
-That took: 3.138 ms.
+That took:  0.968 ms.
+Sets:
+Reconciling [5,3,4] with [6,4,3,10]:
+In the first but not the second: 5
+In the second but not the first: 6,10
+That took:  0.480 ms.
 ```
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,24 @@
-import { Reconciler } from './Reconciler';
+import { Algorithm, Reconciler } from './Reconciler';
 import chalk from 'chalk';
 import now from 'performance-now';
 
 const l1 = [5, 3, 4];
 const l2 = [6, 4, 3, 10];
 
+console.log(`${chalk.cyan('Brute force:')}`);
 var start = now();
 console.log(`Reconciling ${chalk.yellow(`[${l1}]`)} with ${chalk.yellow(`[${l2}]`)}:`);
 let [r1, r2] = Reconciler.reconcile(l1, l2);
 console.log(`In the first but not the second: ${r1}`);
 console.log(`In the second but not the first: ${r2}`);
 var end = now();
+console.log(`${chalk.blue('That took: ')} ${(end - start).toFixed(3)} ms.`);
+
+console.log(`${chalk.cyan('Sets:')}`);
+start = now();
+console.log(`Reconciling ${chalk.yellow(`[${l1}]`)} with ${chalk.yellow(`[${l2}]`)}:`);
+let [f1, f2] = Reconciler.reconcile(l1, l2, Algorithm.Sets);
+console.log(`In the first but not the second: ${f1}`);
+console.log(`In the second but not the first: ${f2}`);
+end = now();
 console.log(`${chalk.blue('That took: ')} ${(end - start).toFixed(3)} ms.`);
