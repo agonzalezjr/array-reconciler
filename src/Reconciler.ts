f826b28a17e0dfd43d569a2073cb1217183d11b5
Andy Gonzalez <andy.gonzalez@sap.com>
2019-06-14 15:00:23 -0400

    optimize with sets

--- src/Reconciler.ts
+++ src/Reconciler.ts
@@ -1,5 +1,6 @@
-enum Algorithm {
+export enum Algorithm {
   BruteForce,
+  Sets,
 }
 
 export class Reconciler {
@@ -11,14 +12,29 @@ export class Reconciler {
     switch (algorithm) {
       case Algorithm.BruteForce:
         return Reconciler.bruteForce(l1, l2);
+      case Algorithm.Sets:
+        return Reconciler.sets(l1, l2);
     }
   }
 
   private static bruteForce(l1: number[], l2: number[]): [number[], number[]] {
-    return [Reconciler.difference(l1, l2), Reconciler.difference(l2, l1)];
+    let difference = function(l1: number[], l2: number[]): number[] {
+      return l1.filter((n: number): boolean => !l2.includes(n));
+    };
+    return [difference(l1, l2), difference(l2, l1)];
   }
 
-  private static difference(l1: number[], l2: number[]): number[] {
-    return l1.filter((n: number): boolean => !l2.includes(n));
+  private static sets(l1: number[], l2: number[]): [number[], number[]] {
+    let s1 = new Set(l1);
+    let s2 = new Set(l2);
+    s1.forEach(
+      (n): void => {
+        if (s2.has(n)) {
+          s2.delete(n);
+          s1.delete(n);
+        }
+      },
+    );
+    return [Array.from(s1), Array.from(s2)];
   }
 }
--- test/Reconciler.spec.ts
+++ test/Reconciler.spec.ts
@@ -1,7 +1,29 @@
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
 
 import { expect } from 'chai';
-import { Reconciler } from '../src/Reconciler';
+import { Reconciler, Algorithm } from '../src/Reconciler';
+
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+}
+
+// creates an array of 100,000 unique numbers in random order
+function randomUniqueArray(size: number): number[] {
+  let a: number[] = [];
+  for (let n = 0; n < size; n++) {
+    a.push(n);
+  }
+  shuffleArray(a);
+  return a;
+}
+
+const n = 100000;
+const a = randomUniqueArray(n);
+a.push(-1);
+const b = randomUniqueArray(n + 1);
 
 describe('Reconciler', () => {
   it('Can reconcile empties', () => {
@@ -10,5 +32,13 @@ describe('Reconciler', () => {
 
   it('Can reconcile', () => {
     expect(Reconciler.reconcile([5, 3, 4], [6, 4, 3, 10])).to.deep.eq([[5], [6, 10]]);
+  });
+
+  it('Can reconcile long ones - BruteForce', () => {
+    expect(Reconciler.reconcile(a, b)).to.deep.eq([[-1], [n]]);
+  });
+
+  it('Can reconcile long ones - Sets', () => {
+    expect(Reconciler.reconcile(a, b, Algorithm.Sets)).to.deep.eq([[-1], [n]]);
   });
 });
